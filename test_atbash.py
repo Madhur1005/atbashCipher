91c654649f25f4235e1655dd3e94f3911333d683
Madhur1005 <madhur_1005@yahoo.com>
2021-05-24 02:20:22 +0530

    adding files

---
+++ test_atbash.py
@@ -0,0 +1,18 @@
+import pytest
+
+def test_file():
+    lookup = {'a': 'z', 'b': 'y', 'c': 'x', 'd': 'w', 'e': 'v', 'f': 'u', 'g': 't', 'h': 's', 'i': 'r', 'j': 'q', 'k': 'p',
+     'l': 'o', 'm': 'n', 'n': 'm', 'o': 'l', 'p': 'k', 'q': 'j', 'r': 'i', 's': 'h', 't': 'g', 'u': 'f', 'v': 'e',
+     'w': 'd', 'x': 'c', 'y': 'b', 'z': 'a', 'A': 'Z', 'B': 'Y', 'C': 'X', 'D': 'W', 'E': 'V', 'F': 'U', 'G': 'T',
+     'H': 'S', 'I': 'R', 'J': 'Q', 'K': 'P', 'L': 'O', 'M': 'N', 'N': 'M', 'O': 'L', 'P': 'K', 'Q': 'J', 'R': 'I',
+     'S': 'H', 'T': 'G', 'U': 'F', 'V': 'E', 'W': 'D', 'X': 'C', 'Y': 'B', 'Z': 'A'}
+    x = "Hello World!"
+    y = "Svool Dliow!"
+    new_x = []
+    for character in x:
+        if character in lookup:
+            character = lookup[character]
+        new_x.append(character)
+    newtxt = ''.join([str(elem) for elem in new_x])
+    assert newtxt == y, "test passed"
+
